1ad40f3acd74d4095e2834243fa3eff0a259185f
Егор Коновалов <eg.konovalov@mail.ru>
2024-05-02 20:04:54 +0300

    feat: upload 2 csharp files

---
+++ result.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+// Длина текста, не считая уже отделённых строк
+int length = text.Length;
+// Позиция, на которой нужно вставить разрыв строки
+int cursor = 0;
+
+// Продолжаем разбивать текст до тех пор, пока он длиннее, чем может поместиться на одной строке
+while (length > Console.WindowWidth)
+{
+    // Получаем новую строку, которая будет влезать в ширину консоли
+    string newLine = text.Substring(cursor, Console.WindowWidth - 4);
+
+    // Находим положение последнего курсора в этой строке
+    int lineLength = newLine.LastIndexOf(' ');
+
+    // Прибавляем к курсору длину новой строки
+    cursor += lineLength;
+    // Вставляем разрыв
+    text = text.Insert(cursor, "\n");
+    // Отнимаем от длины текста длину новой строки
+    length -= lineLength;
+}
+
+// Разбиваем текст на массив строк
+string[] lines = Regex.Split(text, "\r\n|\r|\n");
+
+// Отступ слева будет определяться для каждой строки отдельно
+int left = 0;
+// Определяем отступ сверху для первой строки
+int top = (Console.WindowHeight / 2) - (lines.Length / 2) - 1;
+
+// Находим центр консоли сразу, чтобы не грузить приложение лишними вычислениями
+// Делайте это стоит на свой страх и риск - если пользователь растянет консоль, весь текст поедет
+int center = Console.WindowWidth / 2;
+
+for (int i = 0; i < lines.Length; i++)
+{
+    // Определяем отступ для текущей строки
+    left = center - (lines[i].Length / 2);
+
+    // Меняем положение курсора
+    Console.SetCursorPosition(left, top);
+    // Выводим строку
+    Console.WriteLine(lines[i]);
+
+    // Для каждой новой строки программа будет автоматически считать отступ сверху
+    top = Console.CursorTop;
+}
+
+Console.ReadKey();
---
+++ template.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+restart:
+        
+Console.Write("Write text: ");
+string text = Console.ReadLine();
+Console.Clear();
+        
+int length = text.Length;
+int cursor = 0;
+        
+while (length > Console.WindowWidth)
+{
+    string newLine = text.Substring(cursor, Console.WindowWidth - 4);
+    int lineLength = newLine.LastIndexOf(' ');
+    cursor += lineLength;
+    text = text.Insert(cursor, "\n");
+    length -= lineLength;
+}
+
+string[] lines = Regex.Split(text, "\r\n|\r|\n");
+int left = 0;
+int top = (Console.WindowHeight / 2) - (lines.Length / 2) - 1;
+int center = Console.WindowWidth / 2;
+for (int i = 0; i < lines.Length; i++)
+{
+    left = center - (lines[i].Length / 2);
+    Console.SetCursorPosition(left, top);
+    Console.WriteLine(lines[i]);
+    top = Console.CursorTop;
+}
+
+Console.SetCursorPosition(0, Console.WindowHeight);
+Console.WriteLine("Press any key for restart");
+        
+Console.ReadKey();
+goto restart;
